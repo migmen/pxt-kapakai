446507494b1e45faeaef086dcee15f8d52f00349
Miguel Mendez <migmen@users.noreply.github.com>
2022-11-02 12:10:45 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,9 @@
 /*
 * Extension for Kapakai Robots
 */
-//% color="#A51F22" weight="100" icon="\e04b" block="kapakai"
-//% groups=['LED matrix', 'Control flow', 'others']
+//% color="#A51F22" weight=100 icon="\e04b" block="kapakai"
 namespace kapakai{
  let status = 0;
-
-    /**
-       Computes the square of x
-       @param x the number to square
-   **/
-    //% block="square $x"
-    //% block.loc.fr="$x au carré"
-    //% jsdoc.loc.fr="Calcule le carré de x"
-    //% x.loc.fr="le nombre"
-    function square(x: number): number { }
-
 }
 
 
