50ea5e463181ac55b73cda9fdb176b506651814a
Miguel Mendez <migmen@users.noreply.github.com>
2022-11-05 22:11:26 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
-/*
-*Extension for Kapakai Robots
-*/
-//% color="#A51F22" weight=100 icon="\e04b" block="kapakai"
+
+//% color="#A51F22" weight=100 icon="e04b" block="kapakai"
 namespace kapakai{
  let status = 0;
 }
